c59372b9ccd50bdb2a84247d36c0877ea7663760
m-hamdy1572 <mh3908334@gmail.com>
2022-11-13 17:00:50 +0200

    Mohamed

--- config/passport.js
+++ config/passport.js
@@ -8,7 +8,8 @@ module.exports = (passport) => {
         clientID: GOOGLE_CLIENT_ID, // process.env.GOOGLE_CLIENT_ID
         clientSecret: GOOGLE_CLIENT_SECRET , //process.env.GOOGLE_CLIENT_SECRET
         callbackURL: 'https://storybooks-me1812.herokuapp.com/auth/google/callback',
-        passReqToCallback:true
+        passReqToCallback: true,
+        scope: ['profile', 'email']
     },
         async (accessToken, refreshToken, profile, cb) => {
             const newUser = {
