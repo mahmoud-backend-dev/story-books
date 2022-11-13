918ed9f1ba514678da70cf074c34a2c02a873dea
m-hamdy1572 <mh3908334@gmail.com>
2022-11-14 01:16:53 +0200

    Mohamed

--- routes/index.js
+++ routes/index.js
@@ -17,7 +17,7 @@ router.get('/dashboard', ensureAuth, async (req, res) => {
     try {
         const stories = await Story.find({ user: req.user.id }).lean()
         res.render('dashboard', {
-            name: req.user.firstName,
+            name: req.user,
             stories,
         })
     } catch (error) {   
