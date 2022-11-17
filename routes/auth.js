bb87fd089f20fbc05d6a2bb94490104c8a0b58f0
m-hamdy1572 <mh3908334@gmail.com>
2022-11-17 21:21:41 +0200

    make it better

--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const passport = require('passport')
 const router = express.Router();
-const { ensureAuth } = require('../middleware/auth');
+const { ensureAuth, ensureGuest } = require('../middleware/auth');
 
 // @desc    Auth with Google
 // @route   GET /auth/google
@@ -10,7 +10,7 @@ router.get('/google',  passport.authenticate('google', { scope: ['profile'] }));
 // @desc    Google auth callback
 // @route   GET /auth/google/callback
 router.get(
-    '/google/callback',ensureAuth,
+    '/google/callback',ensureGuest,
     passport.authenticate('google', { successRedirect: '/dashboard', failureRedirect: '/' })
 );
 
