a544192086a7351e98516f30913748a35d3e4be1
m-hamdy1572 <mh3908334@gmail.com>
2022-11-17 21:20:28 +0200

    make it better

--- routes/auth.js
+++ routes/auth.js
@@ -1,26 +1,26 @@
 const express = require('express')
 const passport = require('passport')
-const router = express.Router()
+const router = express.Router();
+const { ensureAuth } = require('../middleware/auth');
 
 // @desc    Auth with Google
 // @route   GET /auth/google
-router.get('/google', passport.authenticate('google', { scope: ['profile'] }));
+router.get('/google',  passport.authenticate('google', { scope: ['profile'] }));
 
 // @desc    Google auth callback
 // @route   GET /auth/google/callback
 router.get(
-    '/google/callback',
+    '/google/callback',ensureAuth,
     passport.authenticate('google', { successRedirect: '/dashboard', failureRedirect: '/' })
 );
 
 // @desc    Logout user
 // @route   /auth/logout
-router.get('/logout', (req, res, next) => {
+router.get('/logout', ensureAuth,(req, res, next) => {
     req.logout((error) => {
         if (error) { return next(error) }
         req.session.destroy();
         res.redirect('/')
     })
 });
-
 module.exports = router
