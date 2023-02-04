dba0ac9332e0627edec9035550f6875ce7ea6f94
mahmoud hamdy <72884628+m-hamdy1572@users.noreply.github.com>
2023-02-04 03:21:01 +0200

    for depoly

--- config/passport.js
+++ config/passport.js
@@ -5,7 +5,7 @@ module.exports = (passport) => {
     passport.use(new GoogleStrategy({
         clientID: process.env.GOOGLE_CLIENT_ID, // GOOGLE_CLIENT_ID
         clientSecret: process.env.GOOGLE_CLIENT_SECRET , // GOOGLE_CLIENT_SECRET
-        //callbackURL: 'http://localhost:1812/auth/google/callback',
+        callbackURL: 'https://story-books-1812.onrender.com/auth/google/callback',
         scope: ['profile']
     },
         async (accessToken, refreshToken, profile, cb) => {
