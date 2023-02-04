d9ad77b9d7c430ddebad0e6bec0965f3ba83fb0e
mahmoud hamdy <72884628+m-hamdy1572@users.noreply.github.com>
2023-02-04 03:15:46 +0200

    for depoly

--- config/db.js
+++ config/db.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 const connectDB = async () => {
     try {
-        const conn = await mongoose.connect(process.env.MONGO_URL); //MONGO_URL
+        await mongoose.connect(process.env.MONGO_URL); //MONGO_URL
         //console.log(`MongoDB Connected: ${conn.connection.host}`)
     } catch (error) {
         console.error(error);
